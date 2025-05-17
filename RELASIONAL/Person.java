4c5ce70d4007269c2e61bc9e75f54d942a733603
dapskuycode <dakerr19@gmail.com>
2025-05-17 11:43:15 +0700

    pert 10

--- RELASIONAL/Person.java
+++ RELASIONAL/Person.java
@@ -1,3 +1,9 @@
+// Pembuat      : M. Daffa' Atstsaqif / 24060123120039
+// Tanggal      : 14 Mei 2025
+// File         : Person.java
+// Deskripsi    : class person
+
+
 public class Person {
     private int id;
     private String name;
